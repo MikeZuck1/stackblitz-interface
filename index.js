854d44ad686f9b52d5acdfdf864b33100de59073
MikeZuck <105445747+MikeZuck1@users.noreply.github.com>
2023-06-22 09:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const readline = require('readline');
 
 const rl = readline.createInterface({ // créer l'interface => readline.createInterface({});  
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout 
 });
 
 rl.question('Entrez un chiffre entre 0 et 10 : ', (chiffre) => {
